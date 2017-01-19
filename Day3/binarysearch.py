4ab7134fe1fae33f4f8c3bee5184c3d06ad2658b
Daniel Sumba <sumbadaniel1@gmail.com>
2017-01-19 16:16:07 +0300

    BinarySearch class, that inherits from the list class the following:  the __init__() takes two integers as parameters, a and b. a is the length of the list to be created and b is the step or difference between consecutive values. It should also initialize an instance variablelength`, that returns the number of elements in the array  Once you are done, create another method called search, it will take just one argument which is the value you are to find. The search function should return a dictionary object, which contains  count, the number of times you function iterated to find the index of the number in question index, the index of the number in question  The search method should implement the binary search algorithm, each time you iterate, you should increase the count, to test how efficient your implementation is.

---
+++ Day3/binarysearch.py
@@ -0,0 +1,40 @@
+class BinarySearch(list):
+
+	def__init__(self, a, b):
+		super(BinarySearch, self).__init__(range(0, (a*b)+1, b)[1:])
+		self.length = a
+
+	def search(self, c):
+		try:
+			c = int(c) # if c is a string digit
+			result = {}
+			count = 0
+			first = 0
+			last = len(self) -1
+			found = False
+
+			if c > self[last] or c < self[first]:
+                return dict([("count", count), ("index", -1)])
+
+			while  first <= last and not found: #  binary search algorithm
+				middle = (first + last)//2
+				if self[middle] == c:
+					found = True
+					result['index'] = middle
+				else:
+					if c < self[middle]:
+						last = middle - 1
+					else:
+						first = midpoint + 1
+				result['count'] = count
+				count +=1
+			print result
+			return result
+		except:
+			raise("Error! Check your target input")
+
+			
+
+
+
+	
